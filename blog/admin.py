ee3a87bf5409213198e32c26486cd807e5d09611
Dudekula Rumina <137053135+dudekularumina@users.noreply.github.com>
2023-10-09 10:16:03 +0530

    Add files via upload

---
+++ blog/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Category, Post
+
+# For configuration of Category Admin
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=('image_tag', 'title', 'description', 'url', 'add_date')
+    search_fields=('title',)
+
+# For configuration of Post Admin
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title',)
+    search_fields=('title',)
+    list_filter=('cat',)
+    list_per_page=50
+
+    class Media:
+        js = ('https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js','js/script.js',)
+
+
+# Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
